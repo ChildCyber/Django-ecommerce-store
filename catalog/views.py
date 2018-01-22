e0a9c0ebcf89e02950a19eafaaf350965b65c472
Mr.Child <lishunsheng2012@gmail.com>
2018-01-22 20:56:40 +0900

    catalog app add urls

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    pass
+
+
+def show_category(request):
+    pass
+
+
+def show_product(request):
+    pass
