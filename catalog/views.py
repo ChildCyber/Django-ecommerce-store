59806dddf84276171ad6b79d193bf31e86045b7b
Mr.Child <lishunsheng2012@gmail.com>
2018-01-22 22:04:49 +0900

    complete catalog.views

--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,27 @@
-# -*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Category, Product
 
 
 # Create your views here.
-def index(request):
-    pass
+def index(request, template_name="catalog/index.html"):
+    page_title = 'Musical Instruments and Sheet Music for Musicians'
+    return render(request, template_name, locals())
 
 
-def show_category(request):
-    pass
+def show_category(request, category_slug, template_name="catalog/category.html"):
+    c = get_object_or_404(Category, slug=category_slug)
+    product = c.product_set.all()
+    page_title = c.name
+    meta_keywords = c.meta_keywords
+    meta_description = c.meta_description
+    return render(request, template_name, context=locals())
 
 
-def show_product(request):
-    pass
+def show_product(request, product_slug, template_name="catalog/product.html"):
+    p = get_object_or_404(Product, slug=product_slug)
+    categories = p.categories.filter(is_active=True)
+    page_title = p.name
+    meta_keywords = p.meta_keywords
+    meta_description = p.meta_description
+    return render(request, template_name, context=locals())
