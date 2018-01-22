2a00a4c4396f6762ffe9c8e70e8fab8b4a379d24
Mr.Child <lishunsheng2012@gmail.com>
2018-01-22 21:11:00 +0900

    utils app add context_processors.py

---
+++ utils/context_processors.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+
+from catalog.models import Category
+
+
+def ecomstore(request):
+    """
+    context processor for the site templates
+    """
+    return {
+            'active_categories': Category.objects.filter(is_active=True),
+            'site_name': settings.SITE_NAME,
+            'meta_keywords': settings.META_KEYWORDS,
+            'meta_description': settings.META_DESCRIPTION,
+            'request': request
+            }
