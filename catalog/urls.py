e0d2aec50577c372b902b3e1ec12841775076a74
Mr.Child <lishunsheng2012@gmail.com>
2018-01-22 21:33:01 +0900

    fix catalog.urls pattern error

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,6 +6,6 @@ from catalog import views
 
 urlpatterns = [
     url(r'^$', views.index, name='catalog_home'),
-    url(r'^category/(?P<category_slug>[-\w+])/$', views.show_category, name='catalog_category'),
-    url(r'^product/(?P<product_slug>[-\w+])/$', views.show_product, name='catalog_product'),
+    url(r'^category/(?P<category_slug>[-\w]+)/$', views.show_category, name='catalog_category'),
+    url(r'^product/(?P<product_slug>[-\w]+)/$', views.show_product, name='catalog_product'),
 ]
