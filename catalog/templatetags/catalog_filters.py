7e03004e49ba0c1007837b088d2b7babc1809c1e
Mr.Child <lishunsheng2012@gmail.com>
2018-01-23 21:23:59 +0900

    catalog app add catalog_filters template tag

---
+++ catalog/templatetags/catalog_filters.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import locale
+
+from django.template import Library
+
+register = Library()
+
+
+@register.filter(name='currency')
+def currency(value):
+    try:
+        locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+    except:
+        locale.setlocale(locale.LC_ALL, '')
+    loc = locale.localeconv()
+    return locale.currency(value, loc['currency_symbol'], grouping=True)
