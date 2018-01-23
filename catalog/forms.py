7ec968a100c893c216096c4dad0569fba5250925
Mr.Child <lishunsheng2012@gmail.com>
2018-01-23 20:27:40 +0900

    catalog.forms add ProductAddToCartForm

--- catalog/forms.py
+++ catalog/forms.py
@@ -14,3 +14,22 @@ class ProductAdminForm(forms.ModelForm):
         if self.cleaned_data['price'] <= 0:
             raise forms.ValidationError('Price must be greater than zero.')
         return self.cleaned_data['price']
+
+
+# 添加购物车表单
+class ProductAddToCartForm(forms.Form):
+    quantity = forms.IntegerField(
+            widget=forms.TextInput(attrs={'size': '2', 'value': '1', 'class': 'quantity', 'maxlength': '5'}),
+            error_messages={'invalid': 'Please enter a valid quantity.'},
+            min_value=1)
+    product_slug = forms.CharField(widget=forms.HiddenInput())
+
+    def __init__(self, request=None, *args, **kwargs):
+        self.request = request
+        super(ProductAddToCartForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        if self.request:
+            if not self.request.session.test_cookie_worked():
+                raise forms.ValidationError("Cookies must be enabled.")
+        return self.cleaned_data
