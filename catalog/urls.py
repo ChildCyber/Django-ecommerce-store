93c0a78379ea0a5975d0e65f721ff07b0fa41e37
Mr.Child <lishunsheng2012@gmail.com>
2018-01-23 20:14:35 +0900

    catalog urls pass extra options to view

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,9 @@ from django.conf.urls import url
 from catalog import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='catalog_home'),
-    url(r'^category/(?P<category_slug>[-\w]+)/$', views.show_category, name='catalog_category'),
-    url(r'^product/(?P<product_slug>[-\w]+)/$', views.show_product, name='catalog_product'),
+    url(r'^$', views.index, {'template_name': 'catalog/index.html'}, name='catalog_home'),
+    url(r'^category/(?P<category_slug>[-\w]+)/$', views.show_category, {'template_name': 'catalog/category.html'},
+        name='catalog_category'),
+    url(r'^product/(?P<product_slug>[-\w]+)/$', views.show_product, {'template_name': 'catalog/product.html'},
+        name='catalog_product'),
 ]
