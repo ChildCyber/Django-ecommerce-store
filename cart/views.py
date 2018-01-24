f1d3f91541620a6866db38681fcf0e6f8f3b48ad
Mr.Child <lishunsheng2012@gmail.com>
2018-01-24 20:26:55 +0900

    complete cart views

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
+from cart import carts
+
 
 # Create your views here.
-def show_cart(request, template_name="cart/cart.html"):
-    pass
+def show_cart(request, template_name="cart/cart_new.html"):
+    if request.method == 'POST':
+        post_data = request.POST.copy()
+        if post_data['submit'] == 'Remove':
+            carts.remove_from_cart(request)
+        if post_data['submit'] == 'Update':
+            carts.update_cart(request)
+    cart_items = carts.get_cart_items(request)
+    page_title = 'Shopping Cart'
+    cart_subtotal = carts.cart_subtotal(request)
+
+    return render(request, template_name, locals())
