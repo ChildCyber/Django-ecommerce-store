3099ffa6351b85915d22fff8a1dfca8020300091
Mr.Child <lishunsheng2012@gmail.com>
2018-01-24 20:57:52 +0900

    catalog app add category_list template tag & modify related html

--- catalog/templatetags/catalog_tags.py
+++ catalog/templatetags/catalog_tags.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.template import Library
 
+from catalog.models import Category
 from cart import carts
 
 register = Library()
@@ -10,3 +11,12 @@ register = Library()
 def cart_box(request):
     cart_item_count = carts.cart_distinct_item_count(request)
     return {'cart_item_count': cart_item_count}
+
+
+@register.inclusion_tag("tags/category_list.html")
+def category_list(request_path):
+    active_categories = Category.objects.filter(is_active=True)
+    return {
+            'active_categories': active_categories,
+            'request_path': request_path,
+            }
--- utils/context_processors.py
+++ utils/context_processors.py
@@ -8,7 +8,7 @@ def ecomstore(request):
     context processor for the site templates
     """
     return {
-            'active_categories': Category.objects.filter(is_active=True),
+            # 'active_categories': Category.objects.filter(is_active=True),
             'site_name': settings.SITE_NAME,
             'meta_keywords': settings.META_KEYWORDS,
             'meta_description': settings.META_DESCRIPTION,
