b4ddc97361cd13531c51662ab45e867cab9ef7af
Mr.Child <lishunsheng2012@gmail.com>
2018-01-25 21:17:21 +0900

    cart views show_cart add Checkout options

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from cart import carts
+from checkout import checkouts
 
 
 # Create your views here.
@@ -11,6 +13,9 @@ def show_cart(request, template_name="cart/cart_new.html"):
             carts.remove_from_cart(request)
         if post_data['submit'] == 'Update':
             carts.update_cart(request)
+        if post_data['submit'] == 'Checkout':
+            checkout_url = checkouts.get_checkout_url(request)
+            return HttpResponseRedirect(checkout_url)
     cart_items = carts.get_cart_items(request)
     page_title = 'Shopping Cart'
     cart_subtotal = carts.cart_subtotal(request)
