534a6b94e97bf38cc2e5e3d0146219b1ba03124e
Mr.Child <lishunsheng2012@gmail.com>
2018-01-25 21:08:39 +0900

    checkout app add checkouts.py

---
+++ checkout/checkouts.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from checkout import google_checkout
+
+
+def get_checkout_url(request):
+    """
+    returns the URL from the checkout module for cart
+    """
+    # use this for Google Checkout API:
+    return google_checkout.get_checkout_url(request)
