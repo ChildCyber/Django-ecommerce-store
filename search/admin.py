206200534c63ec1f5c6bbd9ca2aeb99a37ee2dc8
Mr.Child <lishunsheng2012@gmail.com>
2018-01-28 14:41:14 +0900

    search app register SearchTerm model to admin

--- search/admin.py
+++ search/admin.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
+from search.models import SearchTerm
+
+
 # Register your models here.
+class SearchTermAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'ip_address', 'search_date')
+    list_filter = ('ip_address', 'user', 'q')
+    exclude = ('user',)
+
+
+admin.site.register(SearchTerm, SearchTermAdmin)
