d45d35d227ba56ccd31e4f519c3081d4a095d1bc
Mr.Child <lishunsheng2012@gmail.com>
2018-01-29 21:07:12 +0900

    stats app add PageView, ProductView model

--- stats/models.py
+++ stats/models.py
@@ -1,5 +1,28 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 
 # Create your models here.
+class PageView(models.Model):
+    """
+    model class for tracking the pages that a customer views
+    """
+    class Meta:
+        abstract = True
+
+    date = models.DateTimeField(_(u'创建时间'), auto_now=True)
+    ip_address = models.GenericIPAddressField()
+    user = models.ForeignKey(User, null=True)
+    # 用于追踪每个用户，存储在session中
+    tracking_id = models.CharField(_(u'tracking id'), max_length=50, default='', db_index=True)
+
+
+class ProductView(PageView):
+    """
+    tracks product pages that customer views
+    """
+    product = models.ForeignKey('catalog.Product')
