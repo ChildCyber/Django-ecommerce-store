9789963cf8f9cefca615fdac32e325b841a7d0bd
Mr.Child <lishunsheng2012@gmail.com>
2018-01-29 21:28:24 +0900

    stats.py add user view based recommendation

--- stats/stats.py
+++ stats/stats.py
@@ -4,7 +4,10 @@ import os
 
 from django.conf import settings
 
+from catalog.models import Product
 from search.models import SearchTerm
+
+from .models import ProductView
 
 
 def tracking_id(request):
@@ -57,3 +60,61 @@ def sort_words_by_frequency(some_string):
     sorted_words = sorted(ranked_words, key=lambda word: -word[1])
     # return the list of words, most frequent first
     return [p[0] for p in sorted_words]
+
+
+def log_product_view(request, product):
+    """
+    log the current customer as having viewed the given product instance
+    """
+    t_id = tracking_id(request)
+    try:
+        v = ProductView.objects.get(tracking_id=t_id, product=product)
+        v.save()
+    except ProductView.DoesNotExist:
+        v = ProductView()
+        v.product = product
+        v.ip_address = request.META.get('REMOTE_ADDR')
+        if not request.META.get('REMOTE_ADDR'):
+            v.ip_address = '127.0.0.1'
+        v.user = None
+        v.tracking_id = t_id
+        if request.user.is_authenticated():
+            v.user = request.user
+        v.save()
+
+
+def recommended_from_views(request):
+    """
+    get product recommendations based on products that the customer has viewed;
+    gets list of tracking IDs of other customers who have viewed the products in the current customer's
+    viewed products, and gets products that these other customers also viewed.
+    """
+    t_id = tracking_id(request)
+    # get recently viewed products
+    viewed = get_recently_viewed(request)
+    # if there are previously viewed products, get other tracking ids that have
+    # viewed those products also
+    if viewed:
+        productviews = ProductView.objects.filter(product__in=viewed).values('tracking_id')
+        t_ids = [v['tracking_id'] for v in productviews]
+        # if there are other tracking ids, get the products that those other customers viewed.
+        if t_ids:
+            all_viewed = Product.active.filter(productview__tracking_id__in=t_ids)
+            # if there are other products, get them, excluding the products that the customer
+            # has already viewed.
+            if all_viewed:
+                other_viewed = ProductView.objects.filter(product__in=all_viewed).exclude(product__in=viewed)
+                if other_viewed:
+                    return Product.active.filter(productview__in=other_viewed).distinct()
+
+
+def get_recently_viewed(request):
+    """
+    get settings.PRODUCTS_PER_ROW most recently viewed products for current customer
+    """
+    t_id = tracking_id(request)
+    views = ProductView.objects.filter(tracking_id=t_id).values('product_id').order_by('-date')[
+            0:settings.PRODUCTS_PER_ROW]
+    product_ids = [v['product_id'] for v in views]
+
+    return Product.active.filter(id__in=product_ids)
