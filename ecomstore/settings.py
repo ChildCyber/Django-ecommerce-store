a07bfe3a9af6b40b1d67fddce7a5c8ae3e1655eb
Mr.Child <lishunsheng2012@gmail.com>
2018-01-31 19:45:01 +0900

    create and install marketing app

--- ecomstore/settings.py
+++ ecomstore/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'search.apps.SearchConfig',
     'stats.apps.StatsConfig',
+    'marketing.apps.MarketingConfig',
     # third party
     'tagging',
 ]
