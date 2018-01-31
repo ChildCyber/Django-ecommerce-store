e884fb453462ceeafdcbe5b8e23de44e33006bf6
Mr.Child <lishunsheng2012@gmail.com>
2018-01-31 20:12:14 +0900

    add sitemap

--- marketing/urls.py
+++ marketing/urls.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
+from django.contrib.sitemaps.views import sitemap
 
 from . import views
+from .sitemaps import SITEMAPS
 
 urlpatterns = [
     url(r'^robots\.txt$', views.robots, name='robots'),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': SITEMAPS}),
 ]
